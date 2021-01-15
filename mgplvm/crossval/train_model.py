579d5f70053872c2b51879f4c1f213f7ef14a8bf
K.T. Jensen <ktj21@sagarmatha.eng.cam.ac.uk>
2021-01-15 16:51:58 +0000

    actually fix the errors this time

---
+++ mgplvm/crossval/train_model.py
@@ -0,0 +1,49 @@
+import mgplvm
+from mgplvm import training
+import numpy as np
+import pickle
+import torch
+from torch import optim  
+
+
+def training_params(**kwargs):
+    
+    params = {
+        'max_steps': 1001,
+        'burnin': 150,
+        'callback': None,
+        'optimizer': optim.Adam,
+        'batch_size': None,
+        'ts': None,
+        'print_every': 50,
+        'lrate': 5E-2,
+        'batch_pool': None,
+        'neuron_idxs': None,
+        'hook': None,
+        'n_mc': 32
+    }
+    
+    for key, value in kwargs.items():
+        params[key] = value
+    
+    return params
+
+def train_model(mod, Y, device, params):
+    
+    trained_mod = training.svgp(Y, 
+                           mod, 
+                           device, 
+                           optimizer=params['optimizer'], 
+                           max_steps=int(round(params['max_steps'])), 
+                           burnin=params['burnin'], 
+                           n_mc=params['n_mc'], 
+                           lrate=params['lrate'], 
+                           print_every=params['print_every'],
+                            batch_size = params['batch_size'],
+                           callback = params['callback'],
+                            ts = params['ts'],
+                            batch_pool = params['batch_pool'],
+                            neuron_idxs = params['neuron_idxs'],
+                            hook = params['hook'])
+    
+    return trained_mod
