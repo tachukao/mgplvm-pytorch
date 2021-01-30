e3619df5345cf5ab7e11bd202728e4f1740a9ea2
tachukao <tachu.kao@gmail.com>
2021-01-30 12:03:02 +0000

    added a batching test for svgp elbo

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,7 @@
 import numpy as np
 import torch
 from torch import optim
-import mgplvm
-from mgplvm import kernels, rdist, models, optimisers, syndata, likelihoods
-from mgplvm.manifolds import Torus, Euclid, So3
+import mgplvm as mgp
 import matplotlib.pyplot as plt
 torch.set_default_dtype(torch.float64)
 if torch.cuda.is_available():
@@ -12,9 +10,9 @@ else:
     device = torch.device("cpu")
 
 
-def test_svgp_runs():
+def test_svgplvm_runs():
     """
-    test that svgp runs without explicit check for correctness
+    test that svgplvm runs without explicit check for correctness
     also test that burda log likelihood runs and is smaller than elbo
     """
     d = 1  # dims of latent space
@@ -22,32 +20,32 @@ def test_svgp_runs():
     m = 10  # number of conditions / time points
     n_z = 5  # number of inducing points
     n_samples = 2  # number of samples
-    gen = syndata.Gen(syndata.Euclid(d),
-                      n,
-                      m,
-                      variability=0.25,
-                      n_samples=n_samples)
+    gen = mgp.syndata.Gen(mgp.syndata.Euclid(d),
+                          n,
+                          m,
+                          variability=0.25,
+                          n_samples=n_samples)
     Y = gen.gen_data()
     # specify manifold, kernel and rdist
-    manif = Euclid(m, d)
-    lat_dist = mgplvm.rdist.ReLie(manif, m, n_samples, diagonal=False)
-    kernel = kernels.QuadExp(n, manif.distance)
-    lik = likelihoods.Gaussian(n)
-    lprior = mgplvm.lpriors.Uniform(manif)
+    manif = mgp.manifolds.Euclid(m, d)
+    lat_dist = mgp.rdist.ReLie(manif, m, n_samples, diagonal=False)
+    kernel = mgp.kernels.QuadExp(n, manif.distance)
+    lik = mgp.likelihoods.Gaussian(n)
+    lprior = mgp.lpriors.Uniform(manif)
     z = manif.inducing_points(n, n_z)
-    mod = models.SvgpLvm(n, z, kernel, lik, lat_dist, lprior,
-                         whiten=True).to(device)
+    mod = mgp.models.SvgpLvm(n, z, kernel, lik, lat_dist, lprior,
+                             whiten=True).to(device)
 
     # train model
-    optimisers.svgp.fit(Y,
-                        mod,
-                        device,
-                        optimizer=optim.Adam,
-                        max_steps=5,
-                        burnin=5 / 2E-2,
-                        n_mc=64,
-                        lrate=2E-2,
-                        print_every=1000)
+    mgp.optimisers.svgp.fit(Y,
+                            mod,
+                            device,
+                            optimizer=optim.Adam,
+                            max_steps=5,
+                            burnin=5 / 2E-2,
+                            n_mc=64,
+                            lrate=2E-2,
+                            print_every=1000)
 
     ### test burda log likelihood ###
     LL = mod.calc_LL(torch.tensor(Y).to(device), 128)
@@ -56,15 +54,66 @@ def test_svgp_runs():
 
     assert elbo < LL
 
-    #### test that batching works ####
-    trained_model = optimisers.svgp.fit(Y,
-                                        mod,
-                                        device,
-                                        optimizer=optim.Adam,
-                                        max_steps=5,
-                                        n_mc=64,
-                                        batch_size=int(np.round(m / 2, 0)))
+    #### test that batching with svgplvm runs ####
+    trained_model = mgp.optimisers.svgp.fit(Y,
+                                            mod,
+                                            device,
+                                            optimizer=optim.Adam,
+                                            max_steps=5,
+                                            n_mc=64,
+                                            batch_size=int(np.round(m / 2, 0)))
+
+
+def test_svgp_batching():
+    """
+    test that batching with svgp gives an unbiased estimate of  the true elbo
+    """
+    d = 1  # dims of latent space
+    n = 8  # number of neurons
+    m = 100  # number of conditions / time points
+    n_z = 10  # number of inducing points
+    n_samples = 1  # number of samples
+    gen = mgp.syndata.Gen(mgp.syndata.Euclid(d),
+                          n,
+                          m,
+                          variability=0.25,
+                          n_samples=n_samples)
+    Y = gen.gen_data()
+    manif = mgp.manifolds.Euclid(m, d)
+    lat_dist = mgp.rdist.ReLie(manif, m, n_samples, diagonal=False)
+    kernel = mgp.kernels.QuadExp(n, manif.distance)
+    lik = mgp.likelihoods.Gaussian(n)
+    z = manif.inducing_points(n, n_z)
+    svgp = mgp.models.svgp.Svgp(kernel,
+                                n,
+                                z,
+                                lik,
+                                n_samples=n_samples,
+                                whiten=True)
+    mod = svgp.to(device)
+    lat_dist = lat_dist.to(device)
+    data = torch.tensor(Y).to(device)
+    g = lat_dist.lat_gmu(data).transpose(-1, -2)
+
+    # not batched
+    svgp_lik, svgp_kl = mod.elbo(data, g)
+    elbo = (svgp_lik - svgp_kl).sum().item()
+
+    batch_size = 20
+
+    def for_batch():
+        batch_idxs = np.random.choice(m, batch_size, replace=False)
+        y = data[..., batch_idxs]
+        g = lat_dist.lat_gmu(data, batch_idxs=batch_idxs).transpose(-1, -2)
+        svgp_lik, svgp_kl = mod.elbo(y, g)
+        elbo = ((m / batch_size) * svgp_lik) - svgp_kl
+        return elbo.sum().item()
+
+    estimated_elbo = np.mean([for_batch() for _ in range(2000)])
+
+    assert (np.abs((elbo - estimated_elbo) / (elbo + estimated_elbo)) < 1e-4)
 
 
 if __name__ == '__main__':
-    test_svgp_runs()
+    test_svgplvm_runs()
+    test_svgp_batching()
