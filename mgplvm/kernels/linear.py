7b1116112de2074b839184f360ecdfa581320a59
K.T. Jensen <ktj21@sagarmatha.eng.cam.ac.uk>
2021-02-05 10:17:27 +0000

    yapf and mypy updates

--- mgplvm/kernels/linear.py
+++ mgplvm/kernels/linear.py
@@ -9,7 +9,13 @@ from ..utils import softplus, inv_softplus
 class Linear(Kernel):
     name = "Linear"
 
-    def __init__(self, n: int, d: int, scale=None, learn_scale=False, Y=None, ard = False):
+    def __init__(self,
+                 n: int,
+                 d: int,
+                 scale=None,
+                 learn_scale=False,
+                 Y=None,
+                 ard=False):
         '''
         n is number of neurons/readouts
         d is the dimensionality of the group parameterization
@@ -32,19 +38,21 @@ class Linear(Kernel):
 
         if scale is not None:
             _scale = torch.tensor(scale)
-        elif (Y is not None) and learn_scale:  # <Y^2> = scale * d * <x^2> + <eps^2> = scale * d + sig_noise^2
+        elif (
+                Y is not None
+        ) and learn_scale:  # <Y^2> = scale * d * <x^2> + <eps^2> = scale * d + sig_noise^2
             _scale = torch.tensor(np.sqrt(np.var(
                 Y, axis=(0, 2)) / d)) * 0.5  #assume half signal half noise
         else:
             _scale = torch.ones(n,)  #one per neuron
         self._scale = nn.Parameter(data=_scale, requires_grad=learn_scale)
-        
+
         _ell = inv_softplus(torch.ones(d))
         self._ell = nn.Parameter(data=_ell, requires_grad=ard)
-        
 
     def diagK(self, x: Tensor) -> Tensor:
-        diag = (self.scale_sqr[:, None, None] * (self.prmtize(x)**2)).sum(dim=-2)
+        diag = (self.scale_sqr[:, None, None] *
+                (self.prmtize(x)**2)).sum(dim=-2)
         return diag
 
     def trK(self, x: Tensor) -> Tensor:
@@ -67,14 +75,14 @@ class Linear(Kernel):
         dot = self.dot(self.prmtize(x), self.prmtize(y))
         kxy = self.scale_sqr[:, None, None] * dot
         return kxy
-    
+
     def prmtize(self, x: Tensor) -> Tensor:
         """re-weight the latent dimensions"""
         x = self.ell[:, None] * x
         return x
 
     @property
-    def prms(self) -> Tensor:
+    def prms(self) -> Tuple[Tensor, Tensor]:
         return self.scale, self.ell
 
     @property
@@ -84,15 +92,16 @@ class Linear(Kernel):
     @property
     def scale(self) -> Tensor:
         return self._scale.abs()
-    
+
     @property
     def ell(self) -> Tensor:
         return softplus(self._ell)
 
     @property
     def msg(self):
-        return ('scale {:.3f} | ell {:.3f} |').format(self.scale.mean().item(), self.ell.mean().item())
-    
+        return ('scale {:.3f} | ell {:.3f} |').format(self.scale.mean().item(),
+                                                      self.ell.mean().item())
+
     @staticmethod
     def dot(x: Tensor, y: Tensor) -> Tensor:
         dist = x.transpose(-1, -2).matmul(y)
--- tests/test_ard.py
+++ tests/test_ard.py
@@ -19,28 +19,35 @@ def test_linear_ard():
     test that svgplvm runs without explicit check for correctness
     also test that burda log likelihood runs and is smaller than elbo
     """
-    
+
     x = np
-    
+
     d0 = 2  # dims of true latent space
-    d = d0+2 # gplvm dim
+    d = d0 + 2  # gplvm dim
     n = 15  # number of neurons
     m = 30  # number of conditions / time points
     n_z = 5  # number of inducing points
     n_samples = 1  # number of samples
-    
-    x = np.random.normal(0, 1, size = (n_samples, m, d0)) #generate latents
-    C = np.random.normal(0, 1, size = (n, d0)) #actoor matrix
+
+    x = np.random.normal(0, 1, size=(n_samples, m, d0))  #generate latents
+    C = np.random.normal(0, 1, size=(n, d0))  #actoor matrix
     Y = C @ x.transpose(0, 2, 1)
     assert Y.shape == (n_samples, n, m)
-    
-    sigs = np.random.uniform(0, 0.5, size = n)
-    Y = Y + np.random.normal(0, np.tile(sigs[None, ..., None], (n_samples, 1, m))) #add noise
-    
+
+    sigs = np.random.uniform(0, 0.5, size=n)
+    Y = Y + np.random.normal(0, np.tile(sigs[None, ..., None],
+                                        (n_samples, 1, m)))  #add noise
+
     # specify manifold, kernel and rdist
-    manif = mgp.manifolds.Euclid(m, d) #over-parameterize
-    lat_dist = mgp.rdist.ReLie(manif, m, n_samples, diagonal=True, sigma = 0.2, initialization = 'fa', Y = Y)
-    kernel = mgp.kernels.Linear(n, d, ard = True, learn_scale = False)
+    manif = mgp.manifolds.Euclid(m, d)  #over-parameterize
+    lat_dist = mgp.rdist.ReLie(manif,
+                               m,
+                               n_samples,
+                               diagonal=True,
+                               sigma=0.2,
+                               initialization='fa',
+                               Y=Y)
+    kernel = mgp.kernels.Linear(n, d, ard=True, learn_scale=False)
     lik = mgp.likelihoods.Gaussian(n)
     lprior = mgp.lpriors.Uniform(manif)
     z = manif.inducing_points(n, n_z)
@@ -53,7 +60,7 @@ def test_linear_ard():
                              lat_dist,
                              lprior,
                              whiten=True).to(device)
-    
+
     trained_mod = mgp.optimisers.svgp.fit(torch.tensor(Y).to(device),
                                           mod,
                                           optimizer=optim.Adam,
@@ -63,13 +70,14 @@ def test_linear_ard():
                                           lrate=7.5E-2,
                                           print_every=50)
 
-    
     ells = mod.kernel.ell
     ells = np.sort(ells.detach().cpu().numpy()**(-1))
     print('\n', ells)
 
-    for i in range(d-d0): #more than a standaard deeviaation away from the other ells
-        assert ells[d0+i] > (ells[d0-1]+np.std(ells[:d0]))
+    for i in range(
+            d -
+            d0):  #more than a standaard deeviaation away from the other ells
+        assert ells[d0 + i] > (ells[d0 - 1] + np.std(ells[:d0]))
 
 
 if __name__ == '__main__':
