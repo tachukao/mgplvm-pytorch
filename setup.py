bdbc9be485bb824139a357d548e0129faad9fa14
tachukao <tachu.kao@gmail.com>
2021-03-18 11:15:47 +0000

    yapfed

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ import os
 from setuptools import setup
 from setuptools import find_packages
 
-setup(
-    name='mGPLVM',
-    author='Ta-Chu Kao and Kris Jensen',
-    version='0.0.1',
-    description='Pytorch implementation of mGPLVM',
-    license='MIT',
-    install_requires=['numpy', 'torch==1.7', 'scipy>=1.0.0', 'scikit-learn'],
-    packages=find_packages())
+setup(name='mGPLVM',
+      author='Ta-Chu Kao and Kris Jensen',
+      version='0.0.1',
+      description='Pytorch implementation of mGPLVM',
+      license='MIT',
+      install_requires=['numpy', 'torch==1.7', 'scipy>=1.0.0', 'scikit-learn'],
+      packages=find_packages())
