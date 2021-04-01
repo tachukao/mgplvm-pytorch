1b596e9c898cbc8311c6b5ba59a1515dd6a0df24
K.T. Jensen <ktj21@sagarmatha.eng.cam.ac.uk>
2021-04-01 17:47:45 +0100

    comment on test

--- tests/test_gp_var_dist.py
+++ tests/test_gp_var_dist.py
@@ -117,27 +117,22 @@ def test_GP_lat_prior():
         print(lat_dist.ell)
         jitter = torch.eye(m).to(device)*1e-15
         
-        kl = mod.lat_dist.kl() #computed KL
+        kl = mod.lat_dist.kl() #computed KL from the class
         Kfull = mod.lat_dist.full_cov().to(device) #computed full covariance (n_samples x d x m x m)
-        mu = mod.lat_dist.lat_mu.transpose(-1, -2).to(device) #(n_samples x d x m)
+        mu = mod.lat_dist.lat_mu.transpose(-1, -2).to(device) #(n_samples x d x m); posterior mean
 
+        #construct prior
         lhalf = lat_dist.ell / np.sqrt(2)
         sighalf_sq = 1 * 2**(1/4) * np.pi**(-1/4) * lat_dist.ell**(-1/2)
-
-        print(lhalf.shape, sighalf_sq.shape, dts_sq.shape)
-
         Khalf_prior = sighalf_sq[..., None] * torch.exp( - torch.tensor(dts_sq[:, None, ...]).to(device) / (2*lhalf[..., None]**2) ) 
         Kprior = Khalf_prior @ Khalf_prior #(n_samples x 1 x m x m)
 
-        print(Kfull.shape, Kprior.shape, mu.shape)
-
+        #construct pytorch distributions and compute KL from those
         p = torch.distributions.multivariate_normal.MultivariateNormal(torch.zeros(1).to(device), covariance_matrix=(Kprior+jitter))
         q = torch.distributions.multivariate_normal.MultivariateNormal(mu, covariance_matrix=(Kfull+jitter))
-
         kl_true = torch.distributions.kl.kl_divergence(q, p)
-
+        
         print('computed:\n', kl)
-
         print('true:\n', kl_true)
 
         assert torch.allclose(kl, kl_true)
