b1b8554b9acb39bb47ef6852103ab253e404c854
Jasmine Stone <jts58@cam.ac.uk>
2021-04-05 05:24:16 +0100

    correct the comment

--- mgplvm/likelihoods.py
+++ mgplvm/likelihoods.py
@@ -169,8 +169,8 @@ class Gaussian(Likelihood):
             SVGP likelihood term per MC, neuron, sample (n_mc x n_samples x n)
         """
         n_mc, m = fmu.shape[0], fmu.shape[-1]
-        variance = self.prms
-        inv_variance = 1 / variance[..., None]  #(n)
+        variance = self.prms  #(n)
+        inv_variance = 1 / variance[..., None]
         #print(variance.shape)
         ve1 = -0.5 * log2pi * m  #scalar
         ve2 = -0.5 * torch.log(variance) * m  #(n)
