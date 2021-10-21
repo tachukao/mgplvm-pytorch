132afabe40e47897e865bcd7a427655398c7ec2c
K.T. Jensen <ktj21@sagarmatha.eng.cam.ac.uk>
2021-10-21 19:41:07 +0100

    fix crossval bug

--- mgplvm/crossval/crossval_bgpfa.py
+++ mgplvm/crossval/crossval_bgpfa.py
@@ -91,6 +91,7 @@ def train_cv_bgpfa(Y,
         elif likelihood == 'NegativeBinomial':
             lik = NegativeBinomial(n, Y=Y1)
         elif likelihood == 'Poisson':
+            #print('poisson lik')
             lik = Poisson(n)
         
         mod = Lvgplvm(n, T, d_fit, n_samples, lat_dist, lprior, lik, ard = ard, learn_scale = (not ard), Y = Y1, rel_scale = rel_scale,
@@ -103,6 +104,8 @@ def train_cv_bgpfa(Y,
     n_samples, n, T = Y2.shape
     
     ###rdist: ell
+    manif = Euclid(T, d_fit)
+    lprior = Null(manif)
     ell0 = mod.lat_dist.ell.detach().cpu()
     lat_dist = GP_circ(manif, T, n_samples, fit_ts, _scale=lat_scale, ell = ell0)
     
@@ -116,9 +119,12 @@ def train_cv_bgpfa(Y,
             c, d, total_count = [val.detach().cpu() for val in [mod.obs.likelihood.c, mod.obs.likelihood.d, mod.obs.likelihood.total_count]]
             lik = NegativeBinomial(n, c=c, d=d, total_count=total_count)
         elif likelihood == 'Poisson':
-            lik = Poisson(n)
+            #print('poisson lik')
+            c, d = [val.detach().cpu() for val in [mod.obs.likelihood.c, mod.obs.likelihood.d]]
+            lik = Poisson(n, c = c, d = d)
         
         if Bayesian:
+            #print('bayesian')
             ###obs: q_mu, q_sqrt, _scale, _dim_scale, _neuron_scale
             q_mu, q_sqrt = mod.obs.q_mu.detach().cpu(), mod.obs.q_sqrt.detach().cpu()
             scale, dim_scale, neuron_scale = mod.obs.scale.detach().cpu(), mod.obs.dim_scale.detach().cpu().flatten(), mod.obs.neuron_scale.detach().cpu().flatten()
@@ -127,6 +133,7 @@ def train_cv_bgpfa(Y,
                          Bayesian = True).to(device)
         
         else:
+            #print('not bayesian')
             ###obs: C
             lat_C = mod.obs.C.detach().cpu()
             mod = Lvgplvm(n, T, d_fit, n_samples, lat_dist, lprior, lik, C = lat_C, Bayesian = False).to(device)
@@ -136,7 +143,7 @@ def train_cv_bgpfa(Y,
     for p in mod.parameters():  #no gradients for the remaining parameters
         p.requires_grad = False
 
-    mod.lat_dist.nu.requires_grad = True #latent variational mean
+    mod.lat_dist._nu.requires_grad = True #latent variational mean
     mod.lat_dist._scale.requires_grad = True #latent variational covariance
     if 'circ' in mod.lat_dist.name:
         mod.lat_dist._c.requires_grad = True #latent variational covariance
